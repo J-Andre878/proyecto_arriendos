f8f5cda799ccbf0e0d5fb7c0c6cc90a5944c939c
J-Andre878 <andre878et@gmail.com>
2026-01-31 23:40:33 -0500

    Fix: Add explicit type annotation to properties map in home page

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,7 @@ export default async function Home() {
         {/* Grid de propiedades */}
         {properties.length > 0 ? (
           <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {properties.map((property) => (
+            {properties.map((property: typeof properties[number]) => (
               <PropertyCard
                 key={property.id}
                 id={property.id}
